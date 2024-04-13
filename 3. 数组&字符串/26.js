e884fe146a4fac3c52a7a1be97865a6ef6d92869
gypgyptestmore <2908400267@qq.com>
2024-04-13 16:28:31 +0800

    feat: 双指针原地去重

---
+++ 3. 数组&字符串/26.js
@@ -0,0 +1,15 @@
+// 有序数组原地去重,返回长度
+// 思路💩：存储每个首次出现数字，再标记次数，次数大于1使用splice【增加时间复杂度】
+
+// 使用双指针,当前元素与前一个元素不同，则将 slow 指针向后移动一位，将当前元素放入 slow 指针指向的位置，从而实现了原地去重的效果。
+
+var removeDuplicates = function(nums) {
+    let slow = 0
+    for(let fast = 0;fast < nums.length-1;fast++){
+        if(nums[fast] !== nums[slow]){
+            slow++
+            nums[slow] = nums[fast]//边界
+        }
+    }
+    return slow+1
+};
