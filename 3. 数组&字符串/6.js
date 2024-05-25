f1e59bd93fb703c1f1f9698424a417e05ab40e47
gypgyptestmore <2908400267@qq.com>
2024-05-25 15:58:28 +0800

    z型排列

--- 3. 数组&字符串/6.js
+++ 3. 数组&字符串/6.js
@@ -1,1 +1,29 @@
 // Z字形变换
+// 思路: 输入numRows行，s纵着排列，横着输出
+var convert = function (s, numRows) {
+  if (numRows === 1 || s.length <= numRows) {
+    return s;
+  }
+
+  let rows = new Array(numRows).fill('')
+  let direction = "down"; //存储方向
+  let currentRow = 0; //当前符号在第几个数组
+
+  for (let i = 0; i < s.length; i++) {
+    rows[currentRow] += s[i]; //符号入数组
+
+    if (direction === "down") {
+      currentRow++; //向下，下标自加
+      if (currentRow === numRows - 1) {
+        //直到最后，改变方向
+        direction = "up";
+      }
+    } else {
+      currentRow--; //向上，下标自减
+      if (currentRow === 0) {
+        direction = "down"; //直到触顶，改变方向
+      }
+    }
+  }
+  return rows.join("");
+};
